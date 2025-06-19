abd62214540579e6c42f2cc58f46bc57caece0d8
Kaleb <Kalebreid15@gmail.com>
2025-06-19 15:17:11 -0400

    Bonus Exercise 2 Successful run

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace FactoryPatternExercise2
+﻿using System;
+
+
+namespace FactoryPatternExercise2
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What database would you like to work with?");
+            Console.WriteLine();
+            Console.WriteLine("Type sql");
+            Console.WriteLine("Type mongo");
+            Console.WriteLine("Type list");
+            Console.WriteLine();
+
+            var userInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Invalid input. Please provide a valid database type.");
+                return;
+            }
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            if (db == null)
+            {
+                Console.WriteLine("The specified database type is not supported.");
+                return;
+            }
+
+            
+            db.SaveData();
+            var products = db.LoadData();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: {product.Price}");
+            }
+
+
+
         }
     }
 }
---
+++ FactoryPatternExercise2/SQLDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    class SQLDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product (){ Name = "PS5", Price = 275 },
+            new Product (){ Name = "Empire State building", Price = 480000000 },
+            new Product () { Name = "Soccer Ball", Price = 19 },
+            new Product () { Name = "Corner Flags", Price = 34 },
+            new Product () { Name = "Lexus ES 350", Price = 79500 },
+            new Product () { Name = "Thinking Cap", Price = 16 },
+            new Product () { Name = "Unicycle", Price = 22 },
+            new Product () { Name = "Human Chess Board", Price = 75 },
+            new Product () { Name = "Bling-Bling King", Price = 1200 }
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine(" I am reading data from SQL Data Access.");
+
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine(" I am saving data from SQL Data Access.");
+
+        }
+    }
+}
